1055da2fe75280cfa1915cc1a5e074043e9a34df
Drenor777 <paweljakubczyk1@gmail.com>
2022-01-17 15:27:23 +0100

    Some repair

--- note_app/urls.py
+++ note_app/urls.py
@@ -6,14 +6,22 @@ urlpatterns = [
     path('', views.homepage, name="homepage"),
     path('js/', views.javascript_section, name="js_section"),
     path('jq/', views.j_qery_section, name="jq_section"),
+    path('bs/', views.bootstrap_section, name="bs_section"),
 ]
 
 for js_path in views.js_section_link_and_render_list:
     urlpatterns.append(path('js/' + js_path[0] + '/', js_path[1]))
 
+for jq_path in views.jq_section_link_and_render_list:
+    urlpatterns.append(path('jq/' + jq_path[0] + '/', jq_path[1]))
 
-urlpatterns.append(path('js/js2-data-type/', views.javascript_section))
-# for element in os.listdir('templates/JQery'):
-#     urlpatterns.append(path('jq/' + element, index_renderer(renderer_path='JQery', render_element=element), name=element[4:-5]))
-#
+for bs_path in views.bs_section_link_and_render_list:
+    urlpatterns.append(path('bs/' + bs_path[0] + '/', bs_path[1]))
+
+
+
+# urlpatterns.append(path('js/js2-data-type/', views.javascript_section))
+# # for element in os.listdir('templates/JQery'):
+# #     urlpatterns.append(path('jq/' + element, index_renderer(renderer_path='JQery', render_element=element), name=element[4:-5]))
+# #
 
--- note_app/views.py
+++ note_app/views.py
@@ -13,7 +13,7 @@ def zip_of_pages(directory: str):
     link_list = []
     for element in index_list:
         step += 1
-        title_list.append((str(step) + '. ' + directory + ' ' + element[4:-5]))
+        title_list.append((str(step) + '. ' + directory + ' ' + element[5:-5]).replace('_', ' '))
         link_list.append(link_convention(element))
     index_zip = zip(title_list, link_list)
     return index_zip
@@ -23,6 +23,7 @@ def homepage(request):
     return render(request, 'homepage.html')
 
 
+# JS section
 def javascript_section(request):
     return render(request, 'js_index.html', {'js_zip': zip_of_pages('JavaScript')})
 
@@ -34,5 +35,25 @@ for javascript_file in os.listdir('templates/JavaScript'):
     js_section_link_and_render_list.append((link_convention(javascript_file), js_section_render))
 
 
+# Bootstrap section
+def bootstrap_section(request):
+    return render(request, 'bs_index.html', {'bs_zip': zip_of_pages('Bootstrap')})
+
+
+bs_section_link_and_render_list = []
+for bootstrap_file in os.listdir('templates/Bootstrap'):
+    def bs_section_render(request, bs_file=bootstrap_file, ):
+        return render(request, 'Bootstrap/' + bs_file, {'bs_zip': zip_of_pages('Bootstrap')})
+    bs_section_link_and_render_list.append((link_convention(bootstrap_file), bs_section_render))
+
+
+# JQery section
 def j_qery_section(request):
     return render(request, 'jq_index.html', {'jq_zip': zip_of_pages('JQery')})
+
+
+jq_section_link_and_render_list = []
+for j_qery_file in os.listdir('templates/JQery'):
+    def jq_section_render(request, jq_file=j_qery_file, ):
+        return render(request, 'JQery/' + jq_file, {'jq_zip': zip_of_pages('JQery')})
+    jq_section_link_and_render_list.append((link_convention(j_qery_file), jq_section_render))
